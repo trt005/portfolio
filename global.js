8c3c18a0bf11dc51c7e60e1615d06dfc77dc41e9
trt005 <129990972+trt005@users.noreply.github.com>
2025-10-16 21:59:41 -0700

    added step 4

--- global.js
+++ global.js
@@ -4,10 +4,81 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+// let navLinks = $$('nav a');
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    { url: '', title: 'Home' },
+    {url: 'resume/', title: 'Resume'},
+    { url: 'projects/', title: 'Projects'},
+    { url: 'contact/', title: 'Contacts'},
+    { url: 'https://github.com/trt005', title:'Github'}
+];
+
+const BASE_PATH = (
+    location.hostname === "localhost" ||
+    location.hostname === "127.0.0.1"
+)
+    ? "/"
+    : "/website/";
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    url = !url.startsWith("http") ? BASE_PATH + url : url;
+
+    let a = document.createElement("a");
+    a.href = url;
+    a.textContent = title;
+    
+    a.classList.toggle(
+        "current",
+        a.host === location.host && a.pathname === location.pathname
+    );
+     
+    if (a.host !== location.host) {
+        a.target = "_blank";
+    }
+
+    nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+  <label class="color-scheme">
+    Theme:
+    <select>
+      <option value="light dark">Automatic</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </label>
+  `
 );
 
-currentLink?.classList.add('current');
+let select = document.querySelector(".color-scheme select");
+
+function setColorScheme(colorScheme) {
+    document.documentElement.style.setProperty("color-scheme", colorScheme);
+    localStorage.colorScheme = colorScheme;
+    select.value = colorScheme;
+}
+
+if ("colorScheme" in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+}
+
+select.addEventListener("input", (event) => {
+    console.log("Color scheme changed to", event.target.value);
+    setColorScheme(event.target.value);
+});
