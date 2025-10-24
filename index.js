228f0ce2247fa3215f18b5453493c439f1b0868a
Trinity <trt005@ucsd.edu>
2025-10-24 13:57:20 -0700

    3.0

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const profileStats = document.querySelector('#profile-stats');
 if (profileStats) {
     profileStats.innerHTML = `
     <dl>
-      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-      <dt>Followers:</dt><dd>${githubData.followers}</dd>
-      <dt>Following:</dt><dd>${githubData.following}</dd>
+        <dt>Followers:</dt><dd>${githubData.followers}</dd>
+        <dt>Following:</dt><dd>${githubData.following}</dd>
+        <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+        <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
     </dl>
   `;
 }
