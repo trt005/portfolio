dd3182f7aef1bfa4baa3633b2db9aad180e8eec5
Trinity <trt005@ucsd.edu>
2025-10-23 23:05:26 -0700

    1.5.9

--- projects/projects.js
+++ projects/projects.js
@@ -1,6 +1,7 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
+console.log('Projects data:', projects);
 
 const projectsContainer = document.querySelector('.projects');
 
