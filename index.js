13f81a3db4ea3090b9ef1477f6d6bf0288113ecc
Trinity <trt005@ucsd.edu>
2025-10-24 01:17:03 -0700

    3.5

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-import { fetchJSON, renderProjects} from './global.js';
+import { fetchJSON, renderProjects, fetchHitHubData} from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
+
+const githubData = await fetchGitHubData('trt005');
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
 
 const latestProjects = projects.slice(0, 3);
 
--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,7 @@ import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsTitle = document.querySelector('.projects-title');
-projectsTitle.textContent = `Projects (${projects.length})`;
+projectsTitle.textContent = `${projects.length} Projects`;
 
 const projectsContainer = document.querySelector('.projects');
 
