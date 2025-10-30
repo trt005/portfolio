656b0014bb1ac5caeaedf8297677a6abf822859d
trt005 <129990972+trt005@users.noreply.github.com>
2025-10-30 14:59:00 -0700

    4.1.4

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,46 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('#projects-pie-plot')
+  .append('path')
+  .attr('d', arc)
+  .attr('fill', 'red');
+
+let data = [1, 2];
+
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+let colors = ['gold', 'purple'];
+
+arcs.forEach((arc, idx) => {
+  d3.select('#projects-pie-plot')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
+
 const projects = await fetchJSON('../lib/projects.json');
 
 const projectsTitle = document.querySelector('.projects-title');
