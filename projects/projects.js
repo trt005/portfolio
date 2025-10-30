51cf8adf1744a535ab9d8387cb268c0813803144
trt005 <129990972+trt005@users.noreply.github.com>
2025-10-30 15:23:07 -0700

    1.4.2

--- projects/projects.js
+++ projects/projects.js
@@ -4,30 +4,18 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2];
-
-let total = 0;
-for (let d of data) {
-  total += d;
-};
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-};
-
+let data = [1, 2, 3, 4, 5, 5];
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = ['gold', 'purple'];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 arcs.forEach((arc, idx) => {
-  d3.select('#projects-pie-plot').append('path')
+  d3.select('#projects-pie-plot')
+    .append('path')
     .attr('d', arc)
-    .attr('fill', colors[idx]);
+    .attr('fill', colors(index));
 });
 
 const projects = await fetchJSON('../lib/projects.json');
